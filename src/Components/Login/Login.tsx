fbe451285a336d15199b3b77d6adfe4da53feacd
Natasha-Mauricio <natashamssant@gmail.com>
2025-06-08 16:46:02 -0300

    Link de login atualizado

--- src/Components/Login/Login.tsx
+++ src/Components/Login/Login.tsx
@@ -41,7 +41,7 @@ export default function Login() {
   }
 
   try {
-    const response = await fetch("http://localhost:8080/usuario/login", {
+    const response = await fetch("https://zenite-gs-production.up.railway.app/usuario/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
